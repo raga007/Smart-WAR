e08a10911e43eaa3babacf11905e7b4584a963e4
rsklar <romey.sklar@gmail.com>
2014-07-29 12:52:04 -0400

    initialize location changed stuff

--- app/src/main/java/com/tripadvisor/smartwar/SmartWarInitialize.java
+++ app/src/main/java/com/tripadvisor/smartwar/SmartWarInitialize.java
@@ -2,6 +2,8 @@ package com.tripadvisor.smartwar;
 
 import android.app.Application;
 
+import com.littlefluffytoys.littlefluffylocationlibrary.LocationLibrary;
+
 import net.danlew.android.joda.ResourceZoneInfoProvider;
 
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
@@ -12,6 +14,7 @@ public class SmartWarInitialize extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        LocationLibrary.initialiseLibrary(getBaseContext(), "com.tripadvisor.smartwar");
         CalligraphyConfig.initDefault("fonts/Roboto-Thin.ttf", R.attr.fontPath);
         //Initialize the Joda Time Library
         ResourceZoneInfoProvider.init(this);
