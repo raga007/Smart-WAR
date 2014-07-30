56b940323af32bd31807ee2171ba960e3d548e83
rsklar <romey.sklar@gmail.com>
2014-07-30 13:37:51 -0400

    settings

--- app/src/main/java/Settings.java
+++ app/src/main/java/Settings.java
@@ -3,5 +3,33 @@
  */
 public class Settings {
 
+    private static boolean useSmartTime = true;
+    private static boolean useNearbySuggestions = true;
+    private static boolean useExpiration = true;
+
+    public static boolean isUseSmartTime() {
+        return useSmartTime;
+    }
+
+    public static void setUseSmartTime(boolean useSmartTime) {
+        Settings.useSmartTime = useSmartTime;
+    }
+
+    public static boolean isUseNearbySuggestions() {
+        return useNearbySuggestions;
+    }
+
+    public static void setUseNearbySuggestions(boolean useNearbySuggestions) {
+        Settings.useNearbySuggestions = useNearbySuggestions;
+    }
+
+    public static boolean isUseExpiration() {
+        return useExpiration;
+    }
+
+    public static void setUseExpiration(boolean useExpiration) {
+        Settings.useExpiration = useExpiration;
+    }
+
 
 }
